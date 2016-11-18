8c616d4b63237630dc01020c7e58c01b9e7ca4f2
Dene Wong <denewong@gmail.com>
2016-11-18 13:41:25 -0600

    Update pm3-group20.js

--- pm3-group20.js
+++ pm3-group20.js
@@ -1,1 +1,48 @@
+var order = new Array();
+var quantity = 0;
+var orderTotal = 0;
+
+document.getElementById('itemQuantity').innerHTML = quantity;
+document.getElementById('totalPrice').innerHTML = "$" + orderTotal;
+
+
+function addToOrder( item, price )
+{
+	order.push( item );	
+	changeOrderTotal( price );
+	changeQuantity( 1 );
+}
+
+function removeFromOrder( item, price )  
+{	
+	var found = false;
+	
+	for( var i = 0; i < order.length && !found; i++)  
+	{	
+		if ( order[i] == (item) )
+		{
+			found = true;
+			changeOrderTotal( price );
+			changeQuantity( -1 );
+			order.splice(i, 1)
+			
+		}
+	}
+}
+
+function changeQuantity( amount )
+{
+	var initialQuantity = parseInt( document.getElementById( "itemQuantity" ).innerHTML );
+	
+	var newQuantity = initialQuantity + amount;
+	
+	document.getElementById("itemQuantity").innerHTML = newQuantity;
+}
+
+function changeOrderTotal( price )  
+{
+	var initialPrice = parseInt( document.getElementById( "totalPrice" ).innerHTML.substring(1) );
+	
+	document.getElementById("totalPrice").innerHTML = "$" + ( initialPrice + price );
+}
 
